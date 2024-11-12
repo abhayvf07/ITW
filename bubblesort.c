b86033f8e51759020bb4dc82e18c5fd6f4afa45c
Abhay Vinod Fulsavange <bt23cse020@iiitn.ac.in>
2024-11-12 13:30:49 +0530

    V3

--- bubblesort.c
+++ bubblesort.c
@@ -25,7 +25,8 @@ void printArray(int arr[], int n) {
 int main() {
     int arr[] = {64, 34, 25, 12, 22, 11, 90};
     int n = sizeof(arr) / sizeof(arr[0]);
-    bubbleSort(arr, n);
+    bubbleSort(arr, n)
     printf("Sorted array: \n");
+    printArray(arr, n);
     return 0;
 }
