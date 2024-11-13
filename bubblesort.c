776f2f385528b979f7c7ac48a3b0aa375bd1ef5f
Abhay Vinod Fulsavange <bt23cse020@iiitn.ac.in>
2024-11-13 15:35:39 +0530

    P1

--- bubblesort.c
+++ bubblesort.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-void bubbleSort(int arr[], int n) {
+void bubbleSort(int a[], int n) {
     for (int i = 0; i < n - 1; i++) {
         int swapped = 0;
         for (int j = 0; j < n - i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
+            if (a[j] > a[j + 1]) {
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
                 swapped = 1;
             }
         }
@@ -16,17 +16,17 @@ void bubbleSort(int arr[], int n) {
     }
 }
 
-void printArray(int arr[], int n) {
+void printaay(int a[], int n) {
     for (int i = 0; i < n; i++)
-        printf("%d ", arr[i]);
+        printf("%d ", a[i]);
     printf("\n");
 }
 
 int main() {
-    int arr[] = {64, 34, 25, 12, 22, 11, 90};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    bubbleSort(arr, n)
-    printf("Sorted array: \n");
-    printArray(arr, n);
+    int a[] = {64, 34, 25, 12, 22, 11, 90};
+    int n = sizeof(a) / sizeof(a[0]);
+    bubbleSort(a, n)
+    printf("Sorted aay: \n");
+    printaay(a, n);
     return 0;
 }
