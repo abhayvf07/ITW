d9cc68ba456456151335d3b1b4c96901a8c112f8
Abhay Vinod Fulsavange <bt23cse020@iiitn.ac.in>
2024-11-13 15:37:17 +0530

    P2

--- selectionsort.c
+++ selectionsort.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 
-void selectionSort(int arr[], int n) {
+void selectionSort(int a[], int n) {
     for (int i = 0; i < n - 1; i++) {
         int minIdx = i;
         for (int j = i + 1; j < n; j++) {
-            if (arr[j] < arr[minIdx]) 
+            if (a[j] < a[minIdx]) 
                 minIdx = j;
         }
-        int temp = arr[minIdx];
-        arr[minIdx] = arr[i];
-        arr[i] = temp;
+        int temp = a[minIdx];
+        a[minIdx] = a[i];
+        a[i] = temp;
     }
 }
 
-void printArray(int arr[], int n) {
+void printaay(int a[], int n) {
     for (int i = 0; i < n; i++)
-        printf("%d ", arr[i]);
+        printf("%d ", a[i]);
     printf("\n");
 }
 
 int main() {
-    int arr[] = {64, 25, 12, 22, 11};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    selectionSort(arr, n);
-    printf("Sorted array: \n")
-    printArray(arr, n);
+    int a[] = {64, 25, 12, 22, 11};
+    int n = sizeof(a) / sizeof(a[0]);
+    selectionSort(a, n);
+    printf("Sorted aay: \n")
+    printaay(a, n);
     return 0;
 }
